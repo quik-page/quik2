814099c198547d7162c722b922b4b68ff8cf0926
陈思全 <98014938+siquan001@users.noreply.github.com>
2023-12-03 05:44:36 +0000

    update

--- js/says.js
+++ js/says.js
@@ -3,7 +3,7 @@
   var jinrishici=_REQUIRE_('./api/jinrishici.js');
   var hitokoto=_REQUIRE_('./api/hitokoto.js');
 
-  var sayTypes=['user','jinrishici','hitokoto'];
+//   var sayTypes=['user','jinrishici','hitokoto'];
 
   var sayF=util.element('div',{
     class:"says"
@@ -30,83 +30,71 @@
     sayinfoDialog.close();
   }
 
+  var sayTypes={},nowSay={};
+  function addSayType(details){
+    if(typeof details!='object'||!details||!util.checkDetailsCorrect(details,['name','key','callback'])||typeof details.callback!='function'){
+      throw '参数不正确';
+    }
+    sayTypes[details.key]={
+      name:details.name,
+      callback:details.callback,
+      click:details.click||function(){},
+      menu:details.menu||[
+        {
+          icon:util.getGoogleIcon('e14d'),
+          title:'复制',
+          click:function(){
+            var value=nowSay.say;
+            util.copyText(value);
+          }
+        }
+      ]
+    }
+  }
+
+  function setSayType(key,cb){
+    if(!sayTypes[key]){
+      throw 'key不存在';
+    }
+
+    initsto.set('saytype',key);
+    sayMenu.setList(sayTypes[key].menu);
+    sayI.onclick=sayTypes[key].click
+
+    refsay(key,cb);
+  }
+
+  function refsay(key,cb){
+    if(!sayTypes[key]){
+      throw 'key不存在';
+    }
+    sayI.innerText='...';
+    sayI.title='加载中';
+    sayTypes[key].callback().then(function(say){
+      sayI.innerText=say.say;
+      sayI.setAttribute('title',say.title);
+      nowSay=say;
+      cb&&cb();
+    })
+  }
+
+  function getNowSay(){
+    return nowSay;
+  }
+
+  function openSayDetailsDialog(op){
+    util.query(infd,'ul').innerHTML=(function(){
+      var str='';
+      for(var k in op){
+        str+='<li><b>'+k+':</b> '+op[k]+'</li>';
+      }
+      return str;
+    })();
+    sayinfoDialog.open();
+  }
+
   sayC.onclick=function(e){
     e.stopPropagation();
-    if(nowsaystype=='user'){
-      sayMenu.setList([{
-        icon:util.getGoogleIcon('e3c9'),
-        title:'修改',
-        click:function(){
-          openSaysEditor();
-        }
-      },{
-        icon:util.getGoogleIcon('e14d'),
-        title:'复制',
-        click:function(){
-          // 用document.execCommand 把 sayI的内容复制到剪切板
-          var value=sayI.innerText;
-          util.copyText(value);
-        }
-      }])
-    }else if(nowsaystype=='jinrishici'){
-      sayMenu.setList([{
-        icon:util.getGoogleIcon('e5d5'),
-        title:'刷新',
-        click:function(){
-          sayI.click();
-        }
-      },{
-        icon:util.getGoogleIcon('e14d'),
-        title:'复制',
-        click:function(){
-          // 用document.execCommand 把 sayI的内容复制到剪切板
-          var value=sayI.innerText;
-          util.copyText(value);
-        }
-      },{
-        icon:util.getGoogleIcon('e88e'),
-        title:'诗句详情',
-        click:function(){
-          util.query(infd,'ul').innerHTML=(function(){
-            var str='';
-            for(var k in sjp){
-              str+='<li><b>'+k+':</b> '+sjp[k]+'</li>';
-            }
-            return str;
-          })();
-          sayinfoDialog.open();
-        }
-      }])
-    }else if(nowsaystype=='hitokoto'){
-      sayMenu.setList([{
-        icon:util.getGoogleIcon('e5d5'),
-        title:'刷新',
-        click:function(){
-          sayI.click();
-        }
-      },{
-        icon:util.getGoogleIcon('e14d'),
-        title:'复制',
-        click:function(){
-          // 用document.execCommand 把 sayI的内容复制到剪切板
-          var value=sayI.innerText;
-          util.copyText(value);
-        }
-      },{
-        icon:util.getGoogleIcon('e88e'),
-        title:'一言详情',
-        click:function(){
-          util.query(infd,'ul').innerHTML=(function(){
-            var str='';
-            for(var k in sjp){
-              str+='<li><b>'+k+':</b> '+sjp[k]+'</li>';
-            }
-            return str;
-          })();
-          sayinfoDialog.open();
-        }
-      }])
-    }
     var bo=sayC.getBoundingClientRect()
     sayMenu.setOffset({
       bottom:window.innerHeight-bo.top,
@@ -115,6 +103,131 @@
     sayMenu.show();
   }
 
+  addSayType({
+    key:"user",
+    name:"用户自定义",
+    callback:function(){
+      return new Promise(function(resolve,reject){
+        resolve({
+        say:initsto.get('usersay'),
+        title:"点击修改"});
+      });
+    },
+    click:function(){
+      openSaysEditor();
+    },
+    menu:[{
+      icon:util.getGoogleIcon('e3c9'),
+      title:'修改',
+      click:function(){
+        openSaysEditor();
+      }
+    },{
+      icon:util.getGoogleIcon('e14d'),
+      title:'复制',
+      click:function(){
+        var value=nowSay.say;
+        util.copyText(value);
+      }
+    }]
+  });
+
+  addSayType({
+    key:"hitokoto",
+    name:"Hitokoto",
+    callback:function(){
+      return new Promise(function(resolve,reject){
+        hitokoto.load(function(res){
+          resolve({
+            say:res.hitokoto,
+            from:res.from,
+            uuid:res.uuid,
+            from_who:res.from_who,
+            title:"该一言来自"+res.from+"，由"+res.from_who+"上传"
+          })
+        })
+      })
+    },
+    click:function(){
+      refsay('hitokoto');
+    },
+    menu:[{
+      icon:util.getGoogleIcon('e5d5'),
+      title:'刷新',
+      click:function(){
+        refsay('hitokoto');
+      }
+    },{
+      icon:util.getGoogleIcon('e14d'),
+      title:'复制',
+      click:function(){
+        var value=nowSay.say;
+        util.copyText(value);
+      }
+    },{
+      icon:util.getGoogleIcon('e88e'),
+      title:'一言详情',
+      click:function(){
+        openSayDetailsDialog({
+          'API':"Hitokoto",
+          '内容':nowSay.say,
+          '来源':nowSay.from,
+          '上传者':nowSay.from_who,
+          'UUID':nowSay.uuid
+        })
+      }
+    }]
+  })
+
+  addSayType({
+    key:"jinrishici",
+    name:"今日诗词",
+    callback:function(){
+      return new Promise(function(resolve,reject){
+        jinrishici.load(function(res){
+          resolve({
+            say:res.data.content,
+            author:'('+res.data.origin.dynasty+')'+res.data.origin.author,
+            p_title:'《'+res.data.origin.title+'》',
+            tags:res.data.matchTags.join(' '),
+            content:'<br>'+res.data.origin.content.join('<br>'),
+            title:"摘自"+res.data.origin.dynasty+"·"+res.data.origin.author+"的《"+res.data.origin.title+'》',
+          })
+        })
+      })
+    },
+    click:function(){
+      refsay('jinrishici');
+    },
+    menu:[{
+      icon:util.getGoogleIcon('e5d5'),
+      title:'刷新',
+      click:function(){
+        refsay('jinrishici');
+      }
+    },{
+      icon:util.getGoogleIcon('e14d'),
+      title:'复制',
+      click:function(){
+        var value=nowSay.say;
+        util.copyText(value);
+      }
+    },{
+      icon:util.getGoogleIcon('e88e'),
+      title:'一言详情',
+      click:function(){
+        openSayDetailsDialog({
+          'API':"今日诗词",
+          '内容':nowSay.say,
+          '作者':nowSay.author,
+          '标题':nowSay.p_title,
+          '全部内容':nowSay.content
+        })
+      }
+    }]
+  })
+
+
   if(!initsto.get('usersay')){
     initsto.set('usersay',def);
   }
@@ -122,71 +235,6 @@
   if(!initsto.get('saytype')){
     initsto.set('saytype','user');
   }
-
-  var nowsaystype=null,sjp={};
-  function initSays(saytype){
-    nowsaystype=saytype;
-    if(saytype=='user'){
-      sayI.innerText=initsto.get('usersay');
-      sjp={};
-    }else if(saytype=='jinrishici'){
-      sayI.innerText='...';
-      jinrishici.load(function(res){
-        sayI.innerHTML=res.data.content;
-        sjp={
-          'API':"jinrishici",
-          '内容':res.data.content,
-          '作者':'('+res.data.origin.dynasty+')'+res.data.origin.author,
-          '标题':'《'+res.data.origin.title+'》',
-          '标签':res.data.matchTags.join(' '),
-          '全部内容':'<br>'+res.data.origin.content.join('<br>')
-        }
-      })
-    }else if(saytype=='hitokoto'){
-      sayI.innerText='...';
-      hitokoto.load(function(res){
-        sayI.innerText=res.hitokoto;
-        sjp={
-          'API':"hitokoto",
-          '内容':res.hitokoto,
-          '来自':res.from,
-          '上传者':res.from_who,
-          'UUID':res.uuid
-        }
-      })
-    }
-  }
-
-  sayI.addEventListener('click',function(){
-    if(nowsaystype=='user'){
-      openSaysEditor();
-    }else if(nowsaystype=='jinrishici'){
-      sayI.innerHTML='...';
-      jinrishici.load(function(res){
-        sayI.innerHTML=res.data.content;
-        sjp={
-          'API':"jinrishici",
-          '内容':res.data.content,
-          '作者':'('+res.data.origin.dynasty+')'+res.data.origin.author,
-          '标题':'《'+res.data.origin.title+'》',
-          '标签':res.data.matchTags.join(' '),
-          '全部内容':'<br>'+res.data.origin.content.join('<br>')
-        }
-      })
-    }else if(nowsaystype=='hitokoto'){
-      sayI.innerHTML='...';
-      hitokoto.load(function(res){
-        sayI.innerHTML=res.hitokoto;
-        sjp={
-          'API':"hitokoto",
-          '内容':res.hitokoto,
-          '来自':res.from,
-          '上传者':res.from_who,
-          'UUID':res.uuid
-        }
-      })
-    }
-  });
 
   var sayseditordialog=null;
   function openSaysEditor(){
@@ -224,57 +272,13 @@
     initsto.set('enabled',true);
   }
   if(initsto.get('enabled')){
-    initSays(initsto.get('saytype'));
+    setSayType(initsto.get('saytype'));
   }else{
     sayF.style.display='none';
   }
-  // initSays('jinrishici');
 
-  setting.registerSetting({
-    unit:"一言",
-    title:"显示一言",
-    message:"在页面下方显示一句话",
-    index:0,
-    type:'boolean',
-    get:function(){
-      return initsto.get('enabled');
-    },
-    callback:function(value){
-      initsto.set('enabled',value);
-      if(value){
-        if(!nowsaystype){
-          initSays(initsto.get('saytype'));
-        }
-        sayF.style.display='block';
-      }else{
-        sayF.style.display='none';
-      }
-      return true;
-    }
-  })
-  setting.registerSetting({
-    unit:"一言",
-    title:"一言类型",
-    message:"一言类型设置",
-    index:1,
-    type:'select',
-    get:function(){
-      return initsto.get('saytype');
-    },
-    init:function(){
-      return {
-        'user':"自定义",
-        'jinrishici':"今日诗词",
-        'hitokoto':"随机一言"
-      };
-    },
-    check:function(value){
-      return sayTypes.indexOf(value)!=-1;
-    },
-    callback:function(value){
-      initSays(value);
-      initsto.set('saytype',value);
-      return true;
-    }
-  })
+  return {
+    getNowSay,
+    setSayType,
+  }
 })();
